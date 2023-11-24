402671d068b0f79299d5abb7dbec5ef621374754
colorlessenergy <brianmunozdev@gmail.com>
2023-11-24 11:03:11 -0800

    add functionality to add a task

--- app/_components/AddTaskInput.js
+++ app/_components/AddTaskInput.js
@@ -2,16 +2,41 @@
 
 import { useState } from "react";
 
+import { useTasksContext } from "../_contexts/TasksContext";
+
 const AddTaskInput = () => {
     const [task, setTask] = useState("");
     const handleChange = (event) => {
         setTask(event.currentTarget.value);
     };
 
+    const { tasks, setTasks, id, setId } = useTasksContext();
+    const tasksListIndex = tasks.findIndex((list) => list.active);
+
+    const handleKeyUp = (event) => {
+        if (event.keyCode === 13) {
+            let cloneTasks = JSON.parse(JSON.stringify(tasks));
+            cloneTasks[tasksListIndex].tasks.push({
+                id: id + 1,
+                task: task,
+                done: false,
+            });
+
+            localStorage.setItem("tasks", JSON.stringify(cloneTasks));
+            localStorage.setItem("id", id + 1);
+
+            setTasks(cloneTasks);
+            setId((previousId) => previousId + 1);
+
+            event.currentTarget.value = "";
+        }
+    };
+
     return (
         <input
             type="text"
             onChange={handleChange}
+            onKeyUp={handleKeyUp}
             className="add-task-input mt-2"
         />
     );
