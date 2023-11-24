636c974b1b5b891582504c6f0e27debf64ecf9c4
colorlessenergy <brianmunozdev@gmail.com>
2023-11-24 11:10:49 -0800

    set checked value into localStorage

--- app/_components/Tasks.js
+++ app/_components/Tasks.js
@@ -3,17 +3,30 @@
 import { useTasksContext } from "../_contexts/TasksContext";
 
 const Tasks = () => {
-    const { tasks } = useTasksContext();
+    const { tasks, setTasks } = useTasksContext();
     const tasksList = tasks.find((list) => list.active);
+    const tasksListIndex = tasks.findIndex((list) => list.active);
+
+    const handleOnChange = (event, index) => {
+        let cloneTasks = JSON.parse(JSON.stringify(tasks));
+        cloneTasks[tasksListIndex].tasks[index].done =
+            event.currentTarget.checked;
+
+        localStorage.setItem("tasks", JSON.stringify(cloneTasks));
+
+        setTasks(cloneTasks);
+    };
 
     return (
         <div className="mt-2">
-            {tasksList.tasks.map((task) => {
+            {tasksList.tasks.map((task, index) => {
                 return (
                     <div key={task.id} className="mb-1">
                         <input
                             type="checkbox"
                             id={task.id}
+                            onChange={(event) => handleOnChange(event, index)}
+                            checked={task.done}
                             className="d-none"
                         />
                         <label htmlFor={task.id} className="mr-1"></label>
